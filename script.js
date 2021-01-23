26847a8f765e87fc088c1521d78a71a5ed88116b
CoolHackerBoy <65199906+CoolHackerBoy@users.noreply.github.com>
2021-01-23 11:51:36 -0800

    Titel Screen
    Added a titel screen.  Also, organized everything in index.html.

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 var localPlayer = new Player();
 
+$(document).ready(function(){
+	$('div#mainUI').hide();
+	$('div#usernameContainer').hide();
+})
+
+$(document). on('click','button#startGame',function(){
+	$('div#titalContainer').hide();
+	$('div#usernameContainer').show();
+})
+
 $(document).on('click','button#saveUsername',function(){
 	var username = $('input#username').val();
 	
 	localPlayer.setUsername(username)
+	$('div#usernameContainer').hide();
+	$('div#mainUI').show();
 })
 
 $(document).on('click','button#buyComputer',function(){
