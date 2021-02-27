7f1ffe5e114d976dc9b7e8fb82c00af4f4c71851
CoolHackerBoy <65199906+CoolHackerBoy@users.noreply.github.com>
2021-02-27 10:14:12 -0800

    Containers
    I added different container for computer, library, and store.  I also made a exit button

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ var localPlayer = new Player();
 $(document).ready(function(){
 	$('div#mainUI').hide();
 	$('div#usernameContainer').hide();
+	$('div#libaryContainer').hide();
+	$('div#storeContainer').hide();
+	$('div#computerContainer').hide();
+	$('button#exit').hide();
 })
 
 $(document). on('click','button#startGame',function(){
@@ -18,12 +22,34 @@ $(document).on('click','button#saveUsername',function(){
 	$('div#mainUI').show();
 })
 
+$(document).on('click','div#computer',function(){
+	$('div#mainUI').hide();
+	$('button#exit').show();
+	$('div#computerContainer').show();
+})
+
+$(document).on('click','div#store',function(){
+	$('div#mainUI').hide();
+	$('button#exit').show();
+	$('div#storeContainer').show();
+})
+
 $(document).on('click','button#buyComputer',function(){
 	localPlayer.buyComputer();
+})
+
+$(document).on('click','div#library',function(){
+	$('div#mainUI').hide();
+	$('button#exit').show();
+	$('div#libaryContainer').show();
 })
 
 $(document).on('click','button#createGame',function(){
 	var createGame = $('input#game').val();
 	
 	localPlayer.makeGame(createGame)
+})
+
+$(document).on('click','button#exit',function(){
+	
 })
